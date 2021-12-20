5d1b2ebdd886492db4d6e0e0589b19250c29adc1
mateonidas <teus.polec.other@gmail.com>
2021-12-20 18:16:15 +0100

    feat: Add exception handler for HttpMessageNotReadableException

--- src/main/java/com/teus/projectrpg/controller/exception/RestExceptionHandler.java
+++ src/main/java/com/teus/projectrpg/controller/exception/RestExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.teus.projectrpg.controller.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -30,5 +32,16 @@ public class RestExceptionHandler {
         );
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleException(HttpMessageNotReadableException e){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Invalid value: " + ((InvalidFormatException)e.getCause()).getValue().toString(),
+                new Date(System.currentTimeMillis())
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
